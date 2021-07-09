7706fe1d46a5081a4e2ccfeda84a765bf0c5d0d8
burak <hp_bs@outlook.com>
2021-07-09 20:03:25 +0300

    initial

---
+++ config-override.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = function override(config) {
+	config.resolve = {
+		...config.resolve,
+		alias: {
+			...config.alias,
+			"@hardship": path.resolve(__dirname, "src"),
+			"@components": path.resolve(__dirname, "src/components"),
+			"@hooks": path.resolve(__dirname, "src/hooks"),
+			"@pages": path.resolve(__dirname, "src/pages"),
+			"@services": path.resolve(__dirname, "src/services"),
+			"@utils": path.resolve(__dirname, "src/utils"),
+		},
+	};
+
+	return config;
+};
