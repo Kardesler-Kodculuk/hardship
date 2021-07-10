90d727e1e9b0182da97aa25cc7e0b3c244381716
Ege Emir Özkan <egeemirozkan24@gmail.com>
2021-07-10 16:36:51 +0300

    Add UI.

--- src/controller/Events.ts
+++ src/controller/Events.ts
@@ -1,54 +1,54 @@
-import Game, {states} from "./Game";
+import Game, { states } from "./Game";
 
-const CHANGE:  states = {
-  energy: { total: 0, change: 0 },
-  humans: { total: 0, change: 0 },
-  food: { total: 0, change: 0 },
-  sanity: { total: 0, change: 0 },
-  progress: { total: 0, change: 0 },
+const CHANGE: states = {
+  energy: { total: 0, change: 0, limit: 0 },
+  humans: { total: 0, change: 0, limit: 0 },
+  food: { total: 0, change: 0, limit: 0 },
+  sanity: { total: 0, change: 0, limit: 0 },
+  progress: { total: 0, change: 0, limit: 0 },
 };
 
 export interface Effect {
-    type: "continuous" | "single"
-    to: keyof states
-    value: number
+  type: "continuous" | "single";
+  to: keyof states;
+  value: number;
 }
 
 export interface Event {
-  id: number
-  title: string
-  effects: Effect[]
+  id: number;
+  title: string;
+  effects: Effect[];
 }
 
 export default class Events {
-  events : Event[]
-  current : states[]
+  events: Event[];
+  current: states[];
 
-  constructor(...events:Event[]){
-    this.events = events
-    this.current = []
+  constructor(...events: Event[]) {
+    this.events = events;
+    this.current = [];
   }
 
-  fireEvent(){
-    let e = this.events[~~(Math.random() * this.events.length)]
-    let c = {...CHANGE}
-    let r = {...CHANGE}
-    e.effects.forEach(e=>{
-      if(e.type === "continuous") {
-        c[e.to].change += e.value
-        r[e.to].change += e.value
+  fireEvent() {
+    let e = this.events[~~(Math.random() * this.events.length)];
+    let c = { ...CHANGE };
+    let r = { ...CHANGE };
+    e.effects.forEach((e) => {
+      if (e.type === "continuous") {
+        c[e.to].change += e.value;
+        r[e.to].change += e.value;
       } else {
-        c[e.to].total += e.value
-        r[e.to].total += e.value
+        c[e.to].total += e.value;
+        r[e.to].total += e.value;
       }
-    })
-    this.current.push(r)
-    Game.modifyResources(c)
+    });
+    this.current.push(r);
+    Game.modifyResources(c);
   }
 
-  stopEvent(){
-    this.current.forEach(e=>{
-      Game.modifyResources(e)
-    })
+  stopEvent() {
+    this.current.forEach((e) => {
+      Game.modifyResources(e);
+    });
   }
 }
