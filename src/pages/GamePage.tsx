6a924fadb8a53fb3dcaa3b24ab280a2e87f7c46e
Ege Emir Özkan <egeemirozkan24@gmail.com>
2021-07-11 21:20:17 +0300

    add animejs

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -3,20 +3,37 @@ import { Resources, WelcomeMessage } from "@components";
 import { gameManager } from "@controller";
 import "./GamePage.css";
 import { Ship } from "@components";
+import anime from "animejs";
+
+function fireEngines(background: React.MutableRefObject<null>) {
+  let target = document.getElementsByClassName("Ship")[0];
+  anime({
+    targets: [target], // Animate the ship
+  });
+}
 
 export default function GamePage() {
   const [tick, setTick] = React.useState(false);
   const [isFrozen, setIsFrozen] = React.useState(true);
+  const background = React.useRef(null);
   React.useEffect(() => {
     if (!isFrozen) {
       gameManager.gameLoop(0.2);
       setTimeout(() => setTick((previousTick) => !previousTick), 200);
+      if (
+        gameManager.resources.progress.total >=
+        gameManager.resources.progress.limit
+      ) {
+        setIsFrozen(true);
+        fireEngines()
+      }
     }
   }, [tick, isFrozen]);
   return (
     <div className="gameWindow">
       <img
         className="spaceBackground"
+        ref={background}
         alt="A sliding background full of stars."
         src={"/assets/images/backgrounds/gamebg.jpg"}
       ></img>
