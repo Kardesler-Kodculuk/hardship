5d152bba25d3a8f6b66a60296b1cf9d0dc080b9e
Ege Emir Özkan <egeemirozkan24@gmail.com>
2021-07-11 22:02:41 +0300

    Add finishing animations

--- src/components/Ship.tsx
+++ src/components/Ship.tsx
@@ -3,16 +3,19 @@ import React from "react";
 import ShipRoom from "./Room/ShipRoom";
 import "./Ship.css";
 
-export function Ship() {
+export function Ship(props: {gameOver: boolean}) {
   return (
     <div className="shipWrapper">
+      { props.gameOver ?
       <div className="Ship">
+      
         <div className="ShipRooms">
           {rooms.map((room, index) => {
-            return <ShipRoom key={"shipRoom" + index} room={room} />;
+            return <ShipRoom key={"shipRoom" + index} room={room}/>;
           })}
-        </div>
-      </div>
+        </div>        
+      </div> : null
+      }
     </div>
   );
 }
--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -5,31 +5,58 @@ import "./GamePage.css";
 import { Ship } from "@components";
 import { useGame } from "@services";
 import anime from "animejs";
+import { useHistory } from "react-router";
 
-function fireEngines(background: React.MutableRefObject<null>) {
-  let target = document.getElementsByClassName("Ship")[0];
+function fireEngines(
+  background: React.RefObject<HTMLImageElement>,
+  callback: () => void
+) {
+  let target = document.getElementsByClassName("shipWrapper")[0];
   anime({
     targets: [target], // Animate the ship
+    translateX: 2000,
+    duration: 200,
+    easing: "easeInCirc",
+    complete: () => {
+      callback();
+    },
   });
 }
 
 export default function GamePage() {
   const [tick, setTick] = React.useState(false);
   const game = useGame();
-  const background = React.useRef(null);
+  const background = React.useRef<HTMLImageElement>(null);
+  const history = useHistory();
+  const [gameOver, setGameOver] = React.useState(false);
+  const goToWinScreen = () => {
+    history.push("/winscreen");
+  };
   React.useEffect(() => {
     if (!game?.freezed) {
       gameManager.gameLoop(0.2);
       setTimeout(() => setTick((previousTick) => !previousTick), 200);
+
       if (
         gameManager.resources.progress.total >=
         gameManager.resources.progress.limit
       ) {
-        useGame()?.freeze();
-        fireEngines(background);
+        game?.freeze();
+        setGameOver(true);
+        fireEngines(background, goToWinScreen);
       }
     }
   }, [tick, game?.freezed]);
+  React.useEffect(() => {
+    if (
+      game?.freezed &&
+      background.current?.classList.contains("spaceBackgroundAnimated")
+    ) {
+      background.current?.classList.remove("spaceBackgroundAnimated");
+    } else {
+      background.current?.classList.add("spaceBackgroundAnimated");
+    }
+  }, [game?.freezed]);
   return (
     <div className="gameWindow">
       <img
@@ -38,9 +65,9 @@ export default function GamePage() {
         alt="A sliding background full of stars."
         src={"/assets/images/backgrounds/gamebg.jpg"}
       ></img>
-      <Resources />
-      <Ship />
-      <WelcomeMessage />
+      <Ship gameOver={gameOver} />
+      {!gameOver ? <Resources /> : null}
+      {!gameOver ? <WelcomeMessage /> : null}
     </div>
   );
 }
