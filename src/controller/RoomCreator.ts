66af5b0c8e893be6e5a3793053d8de7b4d7f56ab
Ege Emir Özkan <egeemirozkan24@gmail.com>
2021-07-12 03:06:05 +0300

    Tramslated events.

--- src/controller/RoomCreator.ts
+++ src/controller/RoomCreator.ts
@@ -1,12 +1,12 @@
-import { createEvent, Event } from "./Events"
-import { Room } from "./Room"
+import { createEvent, Event } from "./Events";
+import { Room } from "./Room";
 
 const GeneratorEvents: Event[] = [
   createEvent(
     1000,
-    "Lights Out!",
-    "Lights are sometimes cut to conserve energy",
-    { to: "sanity", type: "continuous", value: 0.25 }
+    "Işıklar Gitti!",
+    "Enerji kıtlığımız yüzünden sık sık ışıkları kesmek zorunda kalıyoruz, mürettebat rahatsız.",
+    { to: "sanity", type: "continuous", value: -0.25 }
   ),
   createEvent(
     1001,
@@ -14,78 +14,73 @@ const GeneratorEvents: Event[] = [
     "Cryogenic pods are failing, putting extra pressure on food supply.",
     { to: "sanity", type: "single", value: -100 }
   ),
-]
+];
 
 const CafeteriaEvents: Event[] = [
   createEvent(
     2000,
-    "A Change in Taste",
-    "With our food supplies dwindling we are starving.",
+    "Tat Değişikliği",
+    "Gıda stoğumuz hızla tükenmekte, çaresizlikten giderek daha kötü yemeklere başvuruyoruz...",
     { to: "sanity", type: "single", value: -100 },
-    { to: "sanity", type: "continuous", value: 0.25 }
+    { to: "sanity", type: "continuous", value: -0.25 }
   ),
-]
+];
 
 const LivingAreaEvents: Event[] = [
   createEvent(
     3000,
-    "Mistakes, Accidents",
-    "A lot of sleepless people mean a lot of accidents.",
+    "Hatalar ve Kazalar",
+    "Bir yığın uykusuz insan bir yığın kazaya sebep oluyor, geminin her yerinde sorunlar oluşuyor.",
     { to: "energy", type: "continuous", value: -0.05 },
     { to: "food", type: "continuous", value: -0.05 },
     { to: "progress", type: "continuous", value: -0.05 },
     { to: "sanity", type: "continuous", value: -0.05 }
   ),
-]
+];
 
 const ControlDeckEvents: Event[] = [
   createEvent(
     4000,
-    "	The Wrong Turn",
-    "You just took the wrong turn! It will cost you.",
+    "Yanlış Yol",
+    "Gemimimiz yanlış yola girdi, bu bize ciddi sorunlara sebep olacak.",
     { to: "progress", type: "single", value: -100 },
-    { to: "sanity", type: "continuous", value: 0.25 }
+    { to: "sanity", type: "continuous", value: -0.25 }
   ),
-]
+];
 
 const ArmoryEvents: Event[] = [
   createEvent(
     4000,
-    "	Aliens!",
-    "Alien attack, bla bla.",
+    "Uzaylılar Saldırıyor",
+    "Uzaylı saldırısına hazırlıksız yakalandık, gemimiz kaçış manevraları yapıp zaman kaybetmek zorunda kaldı.",
     { to: "progress", type: "single", value: -100 },
-    { to: "sanity", type: "continuous", value: 0.25 }
+    { to: "sanity", type: "continuous", value: -0.25 }
   ),
-]
+];
 
 const Generator = new Room(
   "generator",
   "Jenaretör",
   "Generates electricity",
   ...GeneratorEvents
-)
+);
 const Cafeteria = new Room(
   "cafeteria",
   "Kafeterya",
   "Generates food",
   ...CafeteriaEvents
-)
-const LivingArea = new Room("living", "Yatakhane", "", ...LivingAreaEvents)
-const ControlDeck = new Room("control", "Köprü", "", ...ControlDeckEvents)
+);
+const LivingArea = new Room("living", "Yatakhane", "", ...LivingAreaEvents);
+const ControlDeck = new Room("control", "Köprü", "", ...ControlDeckEvents);
 const Armory = new Room(
   "armory",
   "Cephanelik",
   "Defends the ship",
   ...ArmoryEvents
-)
-const ColdRoom = new Room(
-  "cold",
-  "Soğuk Oda",
-  "Stores frozen humans",
-  ...[]
-)
+);
+const ColdRoom = new Room("cold", "Soğuk Oda", "Stores frozen humans", ...[]);
 
-const rooms = [Generator, Cafeteria, LivingArea, ControlDeck, Armory, ColdRoom]
+const rooms = [Generator, Cafeteria, LivingArea, ControlDeck, Armory, ColdRoom];
 
 export {
   rooms,
@@ -95,11 +90,11 @@ export {
   ControlDeck,
   Armory,
   ColdRoom,
-}
+};
 export {
   GeneratorEvents,
   CafeteriaEvents,
   LivingAreaEvents,
   ControlDeckEvents,
   ArmoryEvents,
-}
+};
