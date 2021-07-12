59bd768e3bde01d916bbc26949a68a83ab127298
burak <hp_bs@outlook.com>
2021-07-12 15:45:38 +0300

    deleted hooks

--- config-overrides.js
+++ config-overrides.js
@@ -7,7 +7,6 @@ module.exports = function override(config) {
 			...config.alias,
 			"@hardship": path.resolve(__dirname, "src"),
 			"@components": path.resolve(__dirname, "src/components"),
-			"@hooks": path.resolve(__dirname, "src/hooks"),
 			"@pages": path.resolve(__dirname, "src/pages"),
 			"@controller": path.resolve(__dirname, "src/controller"),
 			"@utils": path.resolve(__dirname, "src/utils"),
